eb12c44f51a1d833d3042dcd1a46c39cdbc8d02f
Bruno Cabral <55759357+CABRALBRUNO000@users.noreply.github.com>
2021-02-11 08:16:27 -0300

    ajustando ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API:'API= https://api-rda-ccbvyd4go.vercel.app/volunteers',
+  API:'https://api-rda-ccbvyd4go.vercel.app/volunteers',
   
   PUBLICKEY: ' public_IbxahIOtljwWt0cQXY+fdYm0kYY=',
   PRIVATEKEY: 'private_J7ty6K/3AxhD8dac2NDc7c8a2uU=',
