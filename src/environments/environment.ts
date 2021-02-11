1579870d05b0ef437476e083e4d5ae17a6bf598a
Bruno Cabral <55759357+CABRALBRUNO000@users.noreply.github.com>
2021-02-11 19:54:27 -0300

    ajustando variáveis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+
+  API:'https://api-rda.vercel.app/volunteers',
   PUBLICKEY: ' public_IbxahIOtljwWt0cQXY+fdYm0kYY=',
   PRIVATEKEY: 'private_J7ty6K/3AxhD8dac2NDc7c8a2uU=',
   URLENDPOINT: 'https://ik.imagekit.io/cimrda',
