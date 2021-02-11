ec3d4c2a86cbb1eeb9462d7fa869028706feae6c
Bruno Cabral <55759357+CABRALBRUNO000@users.noreply.github.com>
2021-02-11 08:17:23 -0300

    ajustando ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   PRIVATEKEY: 'private_J7ty6K/3AxhD8dac2NDc7c8a2uU=',
   URLENDPOINT: 'https://ik.imagekit.io/cimrda',
   IMAGEKITID: 'cimrda',
-  authenticationEndpoint:'http://localhost:3000/crypt'
+  authenticationEndpoint:'https://api-rda-ccbvyd4go.vercel.app/crypt'
   
 
 };
